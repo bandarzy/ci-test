47c775f54c5e3f821433ac3448219219da11acb8
Mateusz Bajko <mateusz_bajko@interia.eu>
2019-04-04 13:29:08 +0200

    run jenkins

--- app/src/main/java/com/example/citest/MainActivity.java
+++ app/src/main/java/com/example/citest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("Important change", "tadam!");
+        Log.i("Important change 2", "build test");
     }
 }
